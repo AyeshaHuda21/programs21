30b19b7f492bc717e409906642f8dbaf851f4426
AyeshaHuda21 <hudakrl901@gmail.com>
2025-02-19 10:43:55 +0530

    Update recursive_age.c

--- DAY3/recursive_age.c
+++ DAY3/recursive_age.c
@@ -1,17 +1,24 @@
-#include <stdio.h>
-
-void displayages(short int* ages,int len, int index){
-    if(index>= len)
-    {
-        return;
-    }
-    printf("%d",ages[index]);
-     printf("\n");
-    displayages(ages,5,index+1);
+#include<stdio.h>
+void displayAges(short int* ages,int length,int index) {
+if(index >= length) {
+return;
 }
-   int main() {
-     short int ages[]={45,42,14,20,70};
-     displayages(ages, 5, 0);
-     return 0;
+printf("%d ",ages[index]);
+displayAges(ages,5,index+1);
+}
 
+void reverseAges(short int* ages,int length,int index) {
+if(index >= length) {
+return;
+}
+reverseAges(ages,5,index+1);
+printf("%d ",ages[index]);
+}
+
+int main() {
+short int ages[] = {45,42,14,20,70};
+displayAges(ages, 5,0);
+printf("\n");
+reverseAges(ages,5,0);
+return 0;
 }
