8fc8ce7ad8026499c04a9bce8fcb2411cbc78a01
AyeshaHuda21 <hudakrl901@gmail.com>
2025-02-19 21:34:06 +0530

    Update assignment_2_patient_level.c

--- DAY3/assignment_2_patient_level.c
+++ DAY3/assignment_2_patient_level.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_PATIENTS 100
+#define MAX_PATIENTS 10
 
 // defining a structure to store patient data
 typedef struct {
-    char name[50];
+    char name[20];
     int level;
 }Patient;
 
 // sorting patients in terms of emergency level
-void sortPatients(Patient patients[], int n) {
+void sort_Patients(Patient patients[], int n) {
     Patient temp;
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (patients[i].level < patients[j].level) {
-                // Swapping
+                
                 temp = patients[i];
                 patients[i] = patients[j];
                 patients[j] = temp;
@@ -24,33 +24,33 @@ void sortPatients(Patient patients[], int n) {
     }
 }
 
-// Main function
+// main function
 int main() {
-    int N;
+    int n;
     
     printf("Enter number of patients: ");
-    scanf("%d", &N);
+    scanf("%d", &n);
 
     Patient patients[MAX_PATIENTS];
 
     // Get the patient data
     printf("Patient Data:\n");
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%d.Enter name: ",i+1);
         scanf("%s", patients[i].name);
         printf("Enter emergency level (1-5): ");
         scanf("%d", &patients[i].level);
     }
 
-    sortPatients(patients, N);
+    sort_Patients(patients, n);
 
     printf("\nSorted by Emergency Level:\n");
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%s - Level %d\n", patients[i].name, patients[i].level);
     }
 
     // Display top 3 critical patients
-    printf("\nTop 3 Critical Patients:\n");
+    printf("\n 3 Most Critical Patients:\n");
     for (int i = 0; i < 3 ; i++) {
         printf("%d. %s\n", i + 1, patients[i].name);
     }
